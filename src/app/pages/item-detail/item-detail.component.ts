78b80e66cada880b833e4bf14cfb84d63065827c
jdegand <70610011+jdegand@users.noreply.github.com>
2024-05-19 19:16:36 -0400

    feat: add to cart button in item detail component

--- src/app/pages/item-detail/item-detail.component.ts
+++ src/app/pages/item-detail/item-detail.component.ts
@@ -3,11 +3,17 @@ import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { ItemFirebaseService } from '../../services/item/item-firebase.service';
 import { AsyncPipe, DatePipe, JsonPipe, NgIf } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { CartService } from '../../services/cart/cart.service';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+import { Item } from '../../interfaces/item.interface';
 
 @Component({
   selector: 'app-item-detail',
   standalone: true,
-  imports: [NgIf, JsonPipe, AsyncPipe, DatePipe],
+  imports: [NgIf, JsonPipe, AsyncPipe, DatePipe, ButtonModule, ToastModule],
+  providers: [MessageService],
   templateUrl: './item-detail.component.html',
   styleUrl: './item-detail.component.css'
 })
@@ -18,6 +24,8 @@ export class ItemDetailComponent {
   item$: Observable<any> | undefined;
 
   itemFirebaseService = inject(ItemFirebaseService);
+  cartService = inject(CartService);
+  messageService = inject(MessageService);
   route = inject(ActivatedRoute);
 
   ngOnInit(): void {
@@ -25,6 +33,34 @@ export class ItemDetailComponent {
       this.id = params['id'];
       this.item$ = this.itemFirebaseService.getItemById(this.id);
     });
+  }
+
+  addToCart(item: Item) {
+
+    // could look into a better way to convert this
+    // using (onClick)="addToCart(item._document.data.value.mapValue.fields)"
+    // item id is missing -> id is added with url parameter value
+    // could get from item if you just pass item vs fields object in template 
+
+    const fixedItem = {
+      categoryId: "",
+      categoryName: "",
+      name: "",
+      note: "",
+      quantity: 0,
+      url: "",
+      id: this.id // param value
+    }
+
+    fixedItem.categoryId = JSON.parse(JSON.stringify(item.categoryId)).stringValue;
+    fixedItem.categoryName = JSON.parse(JSON.stringify(item.categoryName)).stringValue;
+    fixedItem.name = JSON.parse(JSON.stringify(item.name)).stringValue;
+    fixedItem.note = JSON.parse(JSON.stringify(item.note)).stringValue;
+    fixedItem.quantity = Number(JSON.parse(JSON.stringify(item.quantity)).integerValue);
+    fixedItem.url = JSON.parse(JSON.stringify(item.url)).stringValue;
+
+    this.cartService.addToCart(fixedItem);
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: fixedItem.name + " added" });
   }
 
 }
